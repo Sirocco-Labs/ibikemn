b48cca39fcf1d4a3216f3b6494c414bacd675e66
Sirocco Labs <siroccolabs@gmail.com>
2024-03-26 15:49:25 -0500

    structured conditional rendering based on onAuthStateChange response, created adminnavtabs component, modified loginscreen naming conventions and loginuser dispatch object.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,27 +1,83 @@
-import { View, StyleSheet } from "react-native"
-import { Text } from "@rneui/themed"
+import { View, StyleSheet } from "react-native";
+import { Text } from "@rneui/themed";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import PublicUserNavTabs from "../components/PublicUserNavTabs/PublicUserNavTabs"
-import StaffUserNavTabs from "../components/StaffUserNavTabs/StaffUserNavTabs"
+import PublicUserNavTabs from "../components/PublicUserNavTabs/PublicUserNavTabs";
+import StaffUserNavTabs from "../components/StaffUserNavTabs/StaffUserNavTabs";
 import AuthNavTabs from "../components/AuthNavTabs/AuthNavTabs";
+import AdminUserNavTabs from "../components/AdminUserNavTabs/AdminUserNavTabs";
+
 import SplashScreen from "../screens/SplashScreen";
 
+import { supabase } from "../services/supabase/supabase";
+import { getUserQuery } from "../redux/thunks/userThunk";
 
-function App(){
+function App() {
+	const dispatch = useDispatch();
+	const [authenticated, setAuthenticated] = useState("");
+	const [loading, setLoading] = useState(true);
 
-    return(
-        <>
-            <PublicUserNavTabs />
-            {/* <StaffUserNavTabs /> */}
-            {/* <AuthNavTabs /> */}
+	const user = useSelector((store) => store.user);
 
-        </>
-    )
+	useEffect(() => {
+		console.log("APP LOADED");
+		const { data } = supabase.auth.onAuthStateChange((event, session) => {
+			console.log(
+				"Auth State:",
+				"  event: ",
+				event,
+				" session: ",
+				session
+			);
+			if (event === "INITIAL_SESSION") {
+				setLoading(false);
+				setAuthenticated(session?.user.aud);
+				// handle initial session
+			} else if (event === "SIGNED_IN") {
+				setLoading(false);
+				setAuthenticated(session.user.aud);
+				dispatch(getUserQuery(session.user.id));
+				// handle sign in event
+			} else if (event === "SIGNED_OUT") {
+				setLoading(false);
+				setAuthenticated(session?.user.aud);
+				// handle sign out event
+			} else if (event === "PASSWORD_RECOVERY") {
+				setLoading(false);
+				setAuthenticated(session.user.aud);
+				// handle password recovery event
+			} else if (event === "TOKEN_REFRESHED") {
+				setLoading(false);
+				setAuthenticated(session.user.aud);
+				// handle token refreshed event
+			} else if (event === "USER_UPDATED") {
+				setLoading(false);
+				setAuthenticated(session.user.aud);
+				// handle user updated event
+			}
+		});
+		return () => {
+			data.subscription.unsubscribe();
+		};
+	}, []);
+
+	if (loading && !authenticated) {
+		return <SplashScreen />;
+	} else {
+		if (user.is_public) {
+			return <PublicUserNavTabs />;
+		} else if (user.is_staff) {
+			return <StaffUserNavTabs />;
+		} else if (user.is_admin) {
+			return <AdminUserNavTabs />;
+		} else {
+			return <AuthNavTabs />;
+		}
+	}
 }
 
-export default App
+export default App;
 
 const styles = StyleSheet.create({
 	container: {
---
+++ src/components/AdminUserNavTabs/AdminUserNavTabs.jsx
@@ -0,0 +1,89 @@
+import MCIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../../screens/HomeScreen";
+import CommuteScreen from "../../screens/CommuteScreen";
+import UserAccountScreen from "../../screens/UserAccountScreen";
+import BikeListScreen from "../../screens/private/BikeListScreen";
+import ManageCalendarScreen from "../../screens/private/admin/ManageCalendarScreen";
+import ManageDataScreen from "../../screens/private/admin/ManageDataScreen";
+import ManageIncentiveScreen from "../../screens/private/admin/ManageIncentiveScreen";
+import ManageOrganizationScreen from "../../screens/private/admin/ManageOrganizationScreen";
+
+export default function AdminUserNavTabs() {
+	const Tab = createBottomTabNavigator();
+	return (
+		<NavigationContainer>
+			<Tab.Navigator
+				initialRouteName="Home"
+				screenOptions={({ route }) => ({
+					tabBarStyle: {
+						height: 50,
+						padding: 2,
+						// backgroundColor: "#0000ff",
+					},
+					tabBarItemStyle: {
+						margin: 2,
+						padding: 1,
+					},
+					tabBarIcon: ({ focused, color, size }) => {
+						let iconName;
+
+						if (route.name === "Home") {
+							iconName = focused
+								? "home-circle"
+								: "home-circle-outline";
+						} else if (route.name === "Bikes") {
+							iconName = focused ? "bike-fast" : "bike";
+						} else if (route.name === "Organizations") {
+							iconName = focused
+								? "account-group"
+								: "account-group-outline";
+						} else if (route.name === "Commute") {
+							iconName = focused ? "road-variant" : "road";
+						} else if (route.name === "Calendar") {
+							iconName = focused
+								? "calendar-month"
+								: "calendar-month-outline";
+						} else if (route.name === "Data") {
+							iconName = focused
+								? "card-account-details"
+								: "card-account-details-outline";
+						} else if (route.name === "Milestones") {
+							iconName = focused
+								? "trophy-variant"
+								: "trophy-variant-outline";
+						} else if (route.name === "Account") {
+							iconName = focused ? "account" : "account-outline";
+						}
+						return (
+							<MCIcons
+								name={iconName}
+								size={size}
+								color={color}
+							/>
+						);
+					},
+					tabBarActiveTintColor: "#1269A9",
+					tabBarInactiveTintColor: "gray",
+				})}
+			>
+				<Tab.Screen name="Bikes" component={BikeListScreen} />
+				<Tab.Screen
+					name="Organizations"
+					component={ManageOrganizationScreen}
+				/>
+				<Tab.Screen name="Commute" component={CommuteScreen} />
+				<Tab.Screen name="Home" component={HomeScreen} />
+				<Tab.Screen name="Calendar" component={ManageCalendarScreen} />
+				<Tab.Screen name="Data" component={ManageDataScreen} />
+				<Tab.Screen
+					name="Milestones"
+					component={ManageIncentiveScreen}
+				/>
+				<Tab.Screen name="Account" component={UserAccountScreen} />
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
+}

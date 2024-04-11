31d6958b454dceee0e51143040de10881eb719bf
Sirocco Labs <siroccolabs@gmail.com>
2024-04-11 17:17:38 -0500

    troubleshooting git

--- src/screens/RideTrackingScreen.jsx
+++ src/screens/RideTrackingScreen.jsx
@@ -105,7 +105,7 @@ export default function RideTrackingScreen() {
 	};
 
 	const handleEndTime = () => {
-		dispatch(setRideEndTime(new Date().toISOString()));
+		dispatch(setRideEndTime(new Date().toISOString()))
 	};
 
 	return (

c66a826daa4dac822d3e34bc181d807ddb7971e3
Sirocco Labs <siroccolabs@gmail.com>
2024-05-18 15:53:58 -0500

    troubleshooting

--- app.config.js
+++ app.config.js
@@ -1,16 +1,12 @@
 import 'dotenv/config'
 
 export default ({ config }) => ({
- ...config,
-  extra: {
-   ...config.extra,
-    SUPABASE_URL: process.env.NODE_ENV === "production"
-     ? process.env.SUPABASE_URL
-      : process.env.EXPO_PUBLIC_SUPABASE_URL,
-    SUPABASE_ANON_KEY: process.env.NODE_ENV === "production"
-     ? process.env.SUPABASE_ANON_KEY
-      : process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
-  },
+	...config,
+	extra: {
+		...config.extra,
+		SUPABASE_URL: process.env.SUPABASE_URL,
+		SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+	},
 });
 	// expo: {
 	// 	extra: {

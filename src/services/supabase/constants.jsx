96ba57730dc1a73357baa56c764233410a9c6f39
Sirocco Labs <siroccolabs@gmail.com>
2024-05-18 12:53:51 -0500

    trying to test if secrets work

--- src/services/supabase/constants.jsx
+++ src/services/supabase/constants.jsx
@@ -1,10 +1,11 @@
-if (!process.env.EXPO_PUBLIC_SUPABASE_URL) {
-	console.log(
+if (!process.env.EXPO_PUBLIC_SUPABASE_URL || !process.env.SUPABASE_URL) {
+	console.error(
 		"From constants.jsx: ",
 		"Make sure env variables are established!"
 	);
 }
 
-export const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL || "";
+export const SUPABASE_URL =
+	process.env.EXPO_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL;
 export const SUPABASE_ANON_KEY =
-	process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "";
+	process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY;

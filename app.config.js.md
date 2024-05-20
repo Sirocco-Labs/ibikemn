1baffc0c347e00fcb403259c0e1cadb19382974d
Sirocco Labs <siroccolabs@gmail.com>
2024-05-20 15:21:55 -0500

    troubleshooting standalone

---
+++ app.config.js.md
@@ -0,0 +1,21 @@
+import "dotenv/config";
+
+export default ({ config }) => ({
+	...config,
+	extra: {
+		...config.extra,
+		EXPO_PUBLIC_SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL,
+		EXPO_PUBLIC_SUPABASE_ANON_KEY:
+			process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+	},
+});
+// expo: {
+// 	extra: {
+// 		SUPABASE_URL:
+// 			process.env.EXPO_PUBLIC_SUPABASE_URL ||
+// 			process.env.SUPABASE_URL,
+// 		SUPABASE_ANON_KEY:
+// 			process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ||
+// 			process.env.SUPABASE_ANON_KEY,
+// 	},
+// },
--- src/services/supabase/constants.jsx
+++ src/services/supabase/constants.jsx
@@ -1,23 +1,27 @@
-import Constants from "expo-constants";
+// import Constants from "expo-constants";
 
-if (
-	!Constants?.expoConfig?.extra?.SUPABASE_URL ||
-	!Constants?.expoConfig?.extra?.SUPABASE_ANON_KEY
-) {
-	console.error(
-		"From constants.jsx: ",
-		"Make sure env variables are established!"
-	);
-}
-// if (!process.env.EXPO_PUBLIC_SUPABASE_URL || !process.env.SUPABASE_URL) {
+// if (
+// 	!Constants?.expoConfig?.extra?.SUPABASE_URL ||
+// 	!Constants?.expoConfig?.extra?.SUPABASE_ANON_KEY
+// ) {
 // 	console.error(
 // 		"From constants.jsx: ",
 // 		"Make sure env variables are established!"
 // 	);
 // }
+// Constants?.expoConfig?.extra?.SUPABASE_URL;
+// Constants?.expoConfig?.extra?.SUPABASE_ANON_KEY;
 
-export const SUPABASE_URL = Constants?.expoConfig?.extra?.SUPABASE_URL;
-// process.env.EXPO_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL;
+
+if (!process.env.EXPO_PUBLIC_SUPABASE_URL || !process.env.SUPABASE_URL) {
+	console.error(
+		"From constants.jsx: ",
+		"Make sure env variables are established!"
+	);
+}
+
+export const SUPABASE_URL =
+process.env.EXPO_PUBLIC_SUPABASE_URL || '';
+
 export const SUPABASE_ANON_KEY =
-	Constants?.expoConfig?.extra?.SUPABASE_ANON_KEY;
-// process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY;
+process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '';

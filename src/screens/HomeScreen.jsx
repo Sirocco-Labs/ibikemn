0594a0154d881e1126470475f9da77b95470c7bc
Sirocco Labs <siroccolabs@gmail.com>
2024-07-02 17:24:59 -0500

    oooooofdah solution for android FAB flickering on inital app load.

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -237,7 +237,7 @@ export default function HomeScreen() {
 				refreshing={refreshing}
 			>
 				<View style={styles.sectionView}>
-					{/* <Dialog
+					<Dialog
 						isVisible={winner}
 						onBackdropPress={() => {
 							setWinner(false);
@@ -302,7 +302,7 @@ export default function HomeScreen() {
 								</View>
 							</View>
 						</View>
-					</Dialog> */}
+					</Dialog>
 
 					{/* {winner && (
 						<View style={styles.rewardSection}>

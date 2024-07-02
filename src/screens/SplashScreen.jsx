4c9a218cd7f6b6e7303d33a153d7ae828f499145
Sirocco Labs <siroccolabs@gmail.com>
2024-07-02 15:23:00 -0500

    updated splash screen

--- src/screens/SplashScreen.jsx
+++ src/screens/SplashScreen.jsx
@@ -6,17 +6,6 @@ export default function SplashScreen() {
 	const splash = require("../assets/splash.png");
 	return (
 		<View style={styles.flex1}>
-			<Text
-				style={{
-					fontSize: 65,
-					// fontWeight:'700',
-					color: "#1269A9",
-					position: "absolute",
-					top: 150,
-				}}
-			>
-				iBikeMN
-			</Text>
 			<Image
 				source={splash}
 				style={{
